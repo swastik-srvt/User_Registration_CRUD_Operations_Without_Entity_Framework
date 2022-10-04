e37b2a7859665ff02c78a03369a931edfc5b79fe
swastik-srvt <114081047+swastik-srvt@users.noreply.github.com>
2022-10-04 12:19:04 +0530

    Update HomeController.cs

--- User_Registration_CRUD_Operations_Without_Entity_Framework/Controllers/HomeController.cs
+++ User_Registration_CRUD_Operations_Without_Entity_Framework/Controllers/HomeController.cs
@@ -7,27 +7,6 @@ namespace User_Registration_CRUD_Operations_Without_Entity_Framework.Controllers
     /*If you dont want it remove it together with it's views and models*/
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
